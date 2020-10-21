666909da09b4d31a659a80cf1bafb49d27f8255e
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-21 11:16:13 +0700

    CREATE , EDIT sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink, SidebarRoute, SideBtnWrap } from './SidebarElement';
+
+const Sidebar = ({ isOpen, toggle }) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to='ask'>
+            Ask
+          </SidebarLink>
+          <SidebarLink to='answer'>
+            Answer
+          </SidebarLink>
+          <SidebarLink to='profile'>
+            Profile
+          </SidebarLink>
+          <SidebarLink to='register'>
+            Sign Up
+          </SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to='login'>Sign In</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
