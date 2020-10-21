722dd1b4a1a5d5ede050e5b0dac6da9c893b599b
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-21 11:14:34 +0700

    CREATE , EDIT navbar main

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import { Nav , NavbarContainer , NavLogo , MobileIcon , NavMenu , NavItem , NavLinks , NavBtn , NavBtnLink } from './NavbarElements'
+
+
+const Navbar = ({toggle}) => {
+  return (
+    <>
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to='/'>Knola</NavLogo>
+        <MobileIcon onClick={toggle}>
+          <FaBars />
+        </MobileIcon>
+      <NavMenu>
+        <NavItem>
+          <NavLinks to ='/ask'> Ask </NavLinks>
+        </NavItem>
+        <NavItem>
+          <NavLinks to ='/answer'>Answer</NavLinks>
+        </NavItem>
+        <NavItem>
+          <NavLinks to ='/profile'>Profile</NavLinks>
+        </NavItem>
+        <NavItem>
+          <NavLinks to ='/register'>Sign up</NavLinks>
+        </NavItem>
+      </NavMenu>
+
+      <NavBtn>
+        <NavBtnLink to = '/login'>Log In</NavBtnLink>
+      </NavBtn>
+      </NavbarContainer>
+    </Nav>
+    </>
+  )
+}
+
+export default Navbar
