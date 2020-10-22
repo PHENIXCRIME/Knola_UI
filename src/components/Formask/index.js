7973a01106142f302f9fa72086e10cc3719e4a80
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-23 05:32:49 +0700

    CREATE , EDIT form ask

---
+++ src/components/Formask/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Button } from 'react-scroll'
+
+function FormAsk() { 
+  <Conntainer>
+    <Form>
+      <Title> Ask A Question </Title>
+      <a>Question</a>
+      <input type ="text" placeholder = "what the your question"></input>
+      
+      <a>Description</a>
+      <input type = "text" placeholder = "tell more question"></input>
+
+      <Submit>
+        <Button type = "submit">Answer</Button>
+      </Submit>
+    </Form>
+  </Conntainer>
+}
+
+export default FormAsk 
