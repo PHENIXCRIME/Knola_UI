30e0abb412087bd19ad2e8e1e822fab603b0f84b
Charnchon <boatcharnchon@gmail.com>
2020-10-22 13:25:25 +0700

    ADD API TEST

---
+++ src/Test.js
@@ -0,0 +1,21 @@
+import React ,{Suspense}from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Reset } from 'styled-reset'
+import routes from './config/routes'
+import withHelmet from './utils/withHelmet'
+import GlobalStyles from './components/GlobalStyle'
+// import Navbar from './components/Navbar/index'
+import GetTags from './services/GetTags'
+import GetBlogs from './services/GetBlogs'
+import Navbar from './components/Navbar'
+
+function Test() {
+  return (
+    <>
+      <GetTags />
+      <GetBlogs />
+    </>
+  );
+}
+
+export default withHelmet('CAMT DII | Knola') (Test);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Test from './Test';
 
 ReactDOM.render(
   <React.StrictMode>
