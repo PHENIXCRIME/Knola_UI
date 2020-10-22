4713d9d844ced1f947488e421062c1c0c2a1aeab
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-23 04:38:27 +0700

    DELETE form-content-left

--- src/components/Register/Form.js
+++ src/components/Register/Form.js
@@ -12,9 +12,7 @@ const Form = () => {
   return (
     <>
       <div className='form-container'>
-        <div className='form-content-left'>
-          {/* <img className='form-img' src='img/img-2.svg' alt='spaceship' /> */}
-        </div>
+        
         {!isSubmitted ? (
           <FormRegister submitForm={submitForm} />
         ) : (
