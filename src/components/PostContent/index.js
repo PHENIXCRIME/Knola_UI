47b49476bea67f8175d89946f2d255ea5c378fde
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-25 23:53:26 +0700

    IMPORT router-dom , ADD button

--- src/components/PostContent/index.js
+++ src/components/PostContent/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom';
+
 
 const Post = styled.div `
   margin-bottom : 100px ;
@@ -52,25 +54,47 @@ const Description = styled.div `
   p{
     font-size : 15px ;
   }
+`
+const TagAll = styled.button `
+border : #333 1px solid ; 
+border-radius : 10px;
+padding : 1em ; 
+text-align : center ;
 
+ position : relative ; 
+ padding : 15px 30px ; 
+ margin : 10px ; 
+ background-color: #27022d ;
+ color : #fff ;
+ text-decoration : none ;
+ letter-spacing : 1px ; 
+ border : 1px solid #000 ;
+ transition : 0.5s;
+ overflow:hidden;
+ display: flex;
+ justify-content: center;
+
+ :hover{
+   background:#a41ee9 ;
+ }
+
+ ::before { 
+   content : '' ;
+   position : absolute ; 
+   top :0;
+   left: -100%;
+   width:100%; 
+   height:100%;
+   background : linear-gradient(90deg,transparent,#fff,transparent);
+   transition:0.5s;
+ }
+
+ :hover::before{
+  left:100% ;
+ }
 `
 
-const Images = styled.div `
-width: 1031.72x;
-height: 300px;
-background-image: url(${props => props.src});
-background-size: cover;
-background-repeat: no-repeat;
-background-position :center ;
-
-/* border-radius: 10px 10px 0 0; */
-transition: .5s ease;
-&:hover{
-opacity: 0.5;
-}
-`
-
-function PostContent ({src,childern}) { 
+function PostContent () { 
   return (
 
     <Post>
@@ -85,16 +109,13 @@ function PostContent ({src,childern}) {
       </Head>
 
       <Content>
-        
         <Title>
             <h3>What the problem politics in thailand?</h3>
         </Title>
         <Description>
             <p>There have been heavy protests in Thailand over the mouth of the people And the claims of human rights in Thailand</p>
         </Description>
-        <Images src={src}>
-          {childern}
-        </Images>
+        <Link to='/'><TagAll>Click</TagAll></Link>
       </Content>
 
     </Post>
