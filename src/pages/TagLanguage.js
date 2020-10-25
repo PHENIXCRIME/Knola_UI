772e61cc8f54ca6e7eed9fe234bc2fcc5e194303
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-26 00:54:40 +0700

    CREATE ,  EDIT tag all page

---
+++ src/pages/TagLanguage.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import styled from 'styled-components'
+import ProfileHome from '../components/ProfileHome/index'
+import PostHome from '../components/PostHome/index'
+import TagContent from '../components/Tag/index'
+import Contentlanguage from '../components/componenttag/contentlanguage'
+
+const ShowCase = styled.div `
+
+display: grid;
+  grid-template-columns: 0.7fr 1.7fr 0.5fr;
+  grid-template-rows: 1fr;
+  gap: 0px 2em;
+  grid-template-areas: ". . .";
+  margin : 100px 20px 0 20px ;
+`
+const Content__left = styled.div ``
+const Content__center = styled.div ``
+const Content__right = styled.div ``
+
+
+
+function Home () {
+  return (
+    <>
+      <ShowCase>
+          
+          <Content__left>
+          <TagContent/>
+
+          </Content__left>
+
+          <Content__center>
+            <PostHome/>
+            <Contentlanguage/>
+            <Contentlanguage/>
+            <Contentlanguage/>
+            <Contentlanguage/>
+
+
+          </Content__center>
+
+          <Content__right>
+            <ProfileHome/>
+          </Content__right>
+          
+      </ShowCase>
+    </>
+  )
+}
+export default Home
---
+++ src/pages/TagTechnology.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+import ProfileHome from '../components/ProfileHome/index'
+import PostHome from '../components/PostHome/index'
+import TagContent from '../components/Tag/index'
+import Contenttechnology from '../components/componenttag/contenttechnology'
+
+const ShowCase = styled.div `
+
+display: grid;
+  grid-template-columns: 0.7fr 1.7fr 0.5fr;
+  grid-template-rows: 1fr;
+  gap: 0px 2em;
+  grid-template-areas: ". . .";
+  margin : 100px 20px 0 20px ;
+`
+const Content__left = styled.div ``
+const Content__center = styled.div ``
+const Content__right = styled.div ``
+
+
+
+function Home () {
+  return (
+    <>
+      <ShowCase>
+          
+          <Content__left>
+          <TagContent/>
+
+          </Content__left>
+
+          <Content__center>
+            <PostHome/>
+            <Contenttechnology/>
+            <Contenttechnology/>
+            <Contenttechnology/>
+            <Contenttechnology/>
+
+          </Content__center>
+
+          <Content__right>
+            <ProfileHome/>
+          </Content__right>
+          
+      </ShowCase>
+    </>
+  )
+}
+export default Home
