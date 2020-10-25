aa0ac94229ae090c1047cc0af1372113390dbf79
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-25 22:59:05 +0700

    EDIT route new 10 pages

--- src/config/routes.js
+++ src/config/routes.js
@@ -25,6 +25,46 @@ export default {
   Register : { 
     path : '/register' , 
     component : lazy(() => import('../pages/Register'))
+  },
+  Question : { 
+    path : '/question' , 
+    component : lazy(() => import('../pages/Question'))
+  },
+  TagMath : { 
+    path : '/tagmath' , 
+    component : lazy(() => import('../pages/TagMath'))
+  },
+  TagScience : { 
+    path : '/tagscience' , 
+    component : lazy(() => import('../pages/TagScience'))
+  },
+  TagSocial : { 
+    path : '/tagsocial' , 
+    component : lazy(() => import('../pages/TagSocial'))
+  },
+  TagLanguage : { 
+    path : '/taglanguage' , 
+    component : lazy(() => import('../pages/TagLanguage'))
+  },
+  TagHuman : { 
+    path : '/taghuman' , 
+    component : lazy(() => import('../pages/TagHuman'))
+  },
+  TagTechnology : { 
+    path : '/tagtechnology' , 
+    component : lazy(() => import('../pages/TagTechnology'))
+  },
+  TagPolitics : { 
+    path : '/tagpolitics' , 
+    component : lazy(() => import('../pages/TagPolitics'))
+  },
+  TagFood : { 
+    path : '/tagfood' , 
+    component : lazy(() => import('../pages/TagFood'))
+  },
+  TagTravel : { 
+    path : '/tagtravel' , 
+    component : lazy(() => import('../pages/TagTravel'))
   }
   
 }
