5424fe0d6d789a085fc56d160a76be3493571a3c
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-26 11:23:01 +0700

    ADD catagorise.tag , EDIT form

--- src/components/PostAsk/index.js
+++ src/components/PostAsk/index.js
@@ -4,38 +4,61 @@ import styled from 'styled-components'
 
 const ButtonInputCatagorise = styled.select`
   width : 100px ;
-height: 2rem;
-border: 1px solid #707070;
-border-radius: 5px;
-font-size: 15px;
-color: #f04823;
-&::placeholder {
-  color: #707070;
-}
+  height: 2rem;
+  border: 1px solid #707070;
+  border-radius: 5px;
+  font-size: 20px;
+  color: #283c86;
 `
 
 const Optiontag = styled.option`
-  color: #f04823;
+  color: #45a247;
 `
 const Content = styled.div `
   display : flex ; 
-  border : 2px solid #3b3b3b ;
+  /* border : 5px solid #3b3b3b ; */
   margin : 10% ;
   height : 500px ;
+  /* border-radius : 20px ; */
+  /* justify-content : center ; */
 `
 const ContentLeft = styled.div `
-  border : 2px solid #3b3b3b ;
+  border : 5px solid #3b3b3b ;
   width : 50% ;
+  border-radius : 20px ;
+  background : white ;
+  margin : 10px ;
+  
 `
 const ContentRight = styled.div `
-  border : 2px solid #3b3b3b ;
+  border : 5px solid #3b3b3b ;
   width : 50% ;
   display : flex ; 
   flex-direction : column ;
   justify-content : space-around ;
   background : white ;
-
+  border-radius : 20px ;
+  margin : 10px ;
 `
+// const Content = styled.div `
+//   display : flex ; 
+//   border : 2px solid #3b3b3b ;
+//   margin : 10% ;
+//   height : 500px ;
+// `
+// const ContentLeft = styled.div `
+//   border : 2px solid #3b3b3b ;
+//   width : 50% ;
+// `
+// const ContentRight = styled.div `
+//   border : 2px solid #3b3b3b ;
+//   width : 50% ;
+//   display : flex ; 
+//   flex-direction : column ;
+//   justify-content : space-around ;
+//   background : white ;
+
+// `
 const Image = styled.div ``
 const Head = styled.div `
   text-align : center ;
@@ -83,11 +106,42 @@ const FillDescription =styled.input `
 const Button = styled.div `
   display: flex;
   justify-content: space-around;
+  
 `
 const Answer = styled.button `
   width : 100px;
   height : 40px ;
-  
+  text-align : center ;
+  font-size : 15px ;
+  position : relative ;
+  border-radius : 10px;
+  background-color: #11998e ;
+  color : #fff ;
+  text-decoration : none ;
+  letter-spacing : 1px ; 
+  transition : 0.5s;
+  overflow:hidden;
+  width : 100px ; 
+  cursor: pointer;
+  outline : none ;
+  border : none ;
+
+
+ :hover{
+   background-color: #67B26F;
+ }
+
+ ::before { 
+   content : '' ;
+   position : absolute ; 
+   top :0;
+   left: -100%;
+   width:100%; 
+   height:100%;
+   background : linear-gradient(90deg,transparent,#FAFFD1,transparent);
+   transition:0.7s;
+ }
+
 `
 
 function PostAsk () {
@@ -108,16 +162,16 @@ function PostAsk () {
 
           <Tag>
             <p>Tag</p>  
-          <ButtonInputCatagorise placeholder=' fex: Content... '>
-              <Optiontag value='math'>Math</Optiontag>
-              <Optiontag value='math'>Math</Optiontag>
-              <Optiontag value='math'>Math</Optiontag>
-              <Optiontag value='math'>Math</Optiontag>
-              <Optiontag value='math'>Math</Optiontag>
-              <Optiontag value='math'>Math</Optiontag>
-              <Optiontag value='math'>Math</Optiontag>
-              <Optiontag value='math'>Math</Optiontag>
-              <Optiontag value='math'>Math</Optiontag>
+          <ButtonInputCatagorise >
+              <Optiontag value='Math'>Math</Optiontag>
+              <Optiontag value='Science'>Science</Optiontag>
+              <Optiontag value='Social'>Social</Optiontag>
+              <Optiontag value='Language'>Language</Optiontag>
+              <Optiontag value='Human'>Human</Optiontag>
+              <Optiontag value='Teachnology'>Teachnology</Optiontag>
+              <Optiontag value='Politics'>Politics</Optiontag>
+              <Optiontag value='Food'>Food</Optiontag>
+              <Optiontag value='Travel'>Travel</Optiontag>
           </ButtonInputCatagorise>
           </Tag>
 
