bd0a072687213f4982e1ec3c8b9edc56947d80de
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-26 11:21:12 +0700

    EDIT display.grid , button styled

--- src/components/Tag/TagElement.js
+++ src/components/Tag/TagElement.js
@@ -2,34 +2,37 @@ import styled from 'styled-components'
 
 export const TagName = styled.div `
  display : grid ;
- grid-template-columns : repeat(3,1fr) ;
- grid-gap : 5px ;
+ grid-template-columns : repeat(3,0.5fr) ;
+ grid-gap : 3px ;
  box-shadow : 3px 3px 3px #bdc3c7 ;
  background-color : white ;
+ border-radius : 20px ;
 `
 
 
 export const TagAll = styled.button `
-border : #333 1px solid ; 
-border-radius : 10px;
-padding : 1em ; 
-text-align : center ;
+  display: flex;
+  justify-content: center;
+  text-align : center ;
+  font-size : 15px ;
+  position : relative ;
+  border-radius : 10px;
+  padding : 30px ; 
+  margin : 30px ; 
+  background-color: #11998e ;
+  color : #fff ;
+  text-decoration : none ;
+  letter-spacing : 1px ; 
+  transition : 0.5s;
+  overflow:hidden;
+  width : 100px ; 
+  cursor: pointer;
+  outline : none ;
+  border : none ;
 
- position : relative ; 
- padding : 15px 30px ; 
- margin : 10px ; 
- background-color: #27022d ;
- color : #fff ;
- text-decoration : none ;
- letter-spacing : 1px ; 
- border : 1px solid #000 ;
- transition : 0.5s;
- overflow:hidden;
- display: flex;
- justify-content: center;
 
  :hover{
-   background:#a41ee9 ;
+   background-color: #67B26F;
  }
 
  ::before { 
@@ -39,8 +42,8 @@ text-align : center ;
    left: -100%;
    width:100%; 
    height:100%;
-   background : linear-gradient(90deg,transparent,#fff,transparent);
-   transition:0.5s;
+   background : linear-gradient(90deg,transparent,#FAFFD1,transparent);
+   transition:0.7s;
  }
 
  :hover::before{
