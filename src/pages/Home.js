5bb985d88713705074a617dad3a1d66db39cc2e2
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-26 11:20:10 +0700

    EDIT display.grid , HIDE footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
-import Footer from '../components/Footer/index'
+// import Footer from '../components/Footer/index'
 import TagContent from '../components/Tag/index'
 import PostContent from '../components/PostContent/index'
 import PostHome from '../components/PostHome/index'
 import ProfileHome from '../components/ProfileHome/index'
 
 const ShowCase = styled.div `
-
-display: grid;
-  grid-template-columns: 0.7fr 1.7fr 0.5fr;
-  grid-template-rows: 1fr;
-  gap: 0px 2em;
-  grid-template-areas: ". . .";
-  margin : 100px 20px 0 20px ;
+    display: grid;
+    grid-template-columns: 0.8fr 1.6fr 0.5fr;
+    grid-template-rows: 1fr;
+    gap: 0px 2em;
+    grid-template-areas: ". . .";
+    margin: 114px 10px 0 10px;
 `
 const Content__left = styled.div ``
 const Content__center = styled.div ``
 const Content__right = styled.div ``
+
 function Home () {
+
   return (
     <>
-      <ShowCase>
+      <ShowCase >
           
           <Content__left>
             <TagContent/>
           </Content__left>
 
-          <Content__center>
+          <Content__center >
             <PostHome/>
             <PostContent></PostContent>
             <PostContent></PostContent>
             <PostContent></PostContent>
             <PostContent></PostContent>
+
           </Content__center>
 
           <Content__right>
@@ -40,7 +42,7 @@ function Home () {
           </Content__right>
           
       </ShowCase>
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
