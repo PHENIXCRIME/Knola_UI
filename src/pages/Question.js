ae837f2b95b1ab1c4b26b59f1902e16b687e7ffb
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-26 12:26:20 +0700

    EDIT height width

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -4,9 +4,12 @@ import ProfileHome from '../components/ProfileHome/index'
 import PostHome from '../components/PostHome/index'
 import Question from '../components/Question/index'
 
+const Mainask = styled.div`
+  height: 100vh;
+  width: 100%;
+`
 const ShowCase = styled.div `
-
-display: grid;
+  display: grid;
   grid-template-columns: 0.7fr 1.7fr 0.5fr;
   grid-template-rows: 1fr;
   gap: 0px 2em;
@@ -22,22 +25,23 @@ const Content__right = styled.div ``
 function Home () {
   return (
     <>
+    <Mainask>
+
       <ShowCase>
-          
           <Content__left>
-
+            
           </Content__left>
-
           <Content__center>
             <PostHome/>
             <Question/>
           </Content__center>
-
           <Content__right>
             <ProfileHome/>
           </Content__right>
           
       </ShowCase>
+    </Mainask>
+
     </>
   )
 }
