90c8cef22b702981ad36cb94382bf90ca933c599
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-27 05:48:35 +0700

    IMPORT logo navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,6 +3,17 @@ import {FaBars} from 'react-icons/fa'
 import { Nav , NavbarContainer , NavLogo , MobileIcon , NavMenu , NavItem , NavLinks , NavBtn , NavBtnLink , NavBtnLogout , NavUsernameBlock} from './NavbarElements'
 import {LoginContextWrapper} from '../../contexts/LoginContext'
 import GetLoginUser from '../../services/GetLoginUser'
+import Image from '../../assets/flame-75.png'
+import styled from 'styled-components'
+
+const LogoNav = styled.div`
+  background-image: url(${Image});
+  background-position: center;
+  background-size: cover;
+  width: 55px;
+  height: 50px;
+  margin-right: 15px;
+`
 
 const Navbar = ({toggle}) => {
   const {isLogin,setIsLogin,setUsername,username,loginUser, setLoginUser}= useContext(LoginContextWrapper)
@@ -22,7 +33,9 @@ const Navbar = ({toggle}) => {
     <>
     <Nav>
       <NavbarContainer>
-        <NavLogo to='/'>K N O L A</NavLogo>
+        <NavLogo to='/'>
+        <LogoNav src={Image}/>
+          K N O L A</NavLogo>
         <MobileIcon onClick={toggle}>
           <FaBars />
         </MobileIcon>
