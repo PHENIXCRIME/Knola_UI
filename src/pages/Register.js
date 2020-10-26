252054c75d7420bd782f51ab420a01212f949a5f
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-26 11:17:21 +0700

    DELETE backgroundblock , ADD Footer

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,37 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
-// import backgroudImage1 from '../assets/Bigsur.jpg'
-import BackgroundBlock from '../components/BackgroudBlock'
-// import { Title, Subtitle } from '../components/Typography'
 import Form from '../components/Register/Form'
-// import RegisterAPI from '../services/Register'
+import Footer from '../components/Footer/index'
 
 
-const Button = styled.button`
-padding: 1rem 1.5rem ; 
-background-color :transparent ;
-border : #fff solid 1px ; 
-border-radius : 8px ; 
-color : #fff ; 
-margin: 1rem ; 
-cursor : pointer ;
-transition : background-color .5s ;
-text-transform : uppercase ; 
-
-&:hover {
-  background-color : #fff ;
-  color: #000; 
-}
+const Mainask = styled.div`
+  height: 100vh;
+  width: 100%;
 `
-
 function Register () {
   return (
-    <div >
-      {/* <BackgroundBlock src={backgroudImage1}> */}
+    <>
 
-      <Form />
-      {/* </BackgroundBlock> */}
-    </div>
+    <Mainask>
+
+    <Form />
+
+    </Mainask>
+    <Footer />
+    
+    </>
   )
 }
 export default Register
