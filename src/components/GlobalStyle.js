b99dafb73666e9cd3b98fa2d14e8da16975ce41e
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-28 12:20:38 +0700

    ADD height

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -6,12 +6,13 @@ ${reset}
 
  html , body , #root {   
   overflow-x :hidden ;
-  /* height : 100vh ; */
+  height : 100vh ;
   width: 100vw;
   font-family: 'Montserrat', sans-serif;
   font-size: 20px ; 
   scroll-behavior : smooth ;
   background: linear-gradient(155deg, rgba(103,178,111,1) 0%, rgba(76,162,205,1) 100%);
+  back
 }
 
 a { 
